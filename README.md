871db38693c7ae92088fc48d6f1438d894eec65e
idesyatov <dev@null.gnu>
2025-01-02 22:59:10 +0300

    some changes

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # SSLWatch
 
-SSLWatch is a simple command-line tool to check the SSL certificate of a domain. It retrieves and displays information about the SSL certificate, including the subject, issuer, expiration date, and the number of days remaining until expiration.
+SSLWatch is a simple command-line tool to check the SSL certificate of a domain or a local certificate file. It retrieves and displays information about the SSL certificate, including the subject, issuer, expiration date, and the number of days remaining until expiration.
 
 ## Usage
 
-To use SSLWatch, you need to specify the domain you want to check. You can also optionally specify a port and an IP address. Additionally, you can use the `-short` flag to output only the number of days remaining until the certificate expires.
+To use SSLWatch, you need to specify either the domain you want to check or the path to a local certificate file. You can also optionally specify a port and an IP address. Additionally, you can use the `-short` flag to output only the number of days remaining until the certificate expires.
 
 ### Command Line Arguments
 
-- `-domain <domain>`: The domain to check (required).
+- `-domain <domain>`: The domain to check (required if `-certfile` is not specified).
+- `-certfile <path>`: The path to the local certificate file (required if `-domain` is not specified).
 - `-port <port>`: The port to connect to (default is 443).
 - `-ipaddr <ipaddr>`: The IP address to connect to (optional).
 - `-short`: Output only the number of days remaining until certificate expiration (optional).
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "crypto/tls"
+    "crypto/x509"
+    "encoding/pem"
     "flag"
     "fmt"
     "log"
@@ -9,61 +11,88 @@ import (
     "time"
 )
 
-// it's just an implementation of a shell command with some extensions
-// echo | openssl s_client -connect example.com:443 | openssl x509 -noout -dates
 func main() {
     // Define flags
     domain := flag.String("domain", "", "Domain to check")
+    certFile := flag.String("certfile", "", "Path to the local certificate file")
     port := flag.String("port", "443", "Port to connect (default is 443)")
     ipaddr := flag.String("ipaddr", "", "IP address to connect to (optional)")
     short := flag.Bool("short", false, "Output only the number of days remaining until certificate expiration")
     flag.Parse()
 
-    // Check if the domain is specified
-    if *domain == "" {
-        fmt.Println("Usage: SSLWatch -domain <domain> [-port <port>] [-ipaddr <ipaddr>] [-short]")
+    // Check if either domain or certfile is specified
+    if *domain == "" && *certFile == "" {
+        fmt.Println("Usage: SSLWatch -domain <domain> [-port <port>] [-ipaddr <ipaddr>] [-short] | -certfile <path>")
         os.Exit(1)
     }
 
-    // Determine the address to connect to
-    address := fmt.Sprintf("%s:%s", *domain, *port)
-    if *ipaddr != "" {
-        address = fmt.Sprintf("%s:%s", *ipaddr, *port)
+    var cert *x509.Certificate
+    var err error
+
+    // If certFile is provided, load the certificate from the file
+    if *certFile != "" {
+        cert, err = loadCertificate(*certFile)
+        if err != nil {
+            log.Fatalf("Failed to load certificate: %v", err)
+        }
+    } else {
+        // Determine the address to connect to
+        address := fmt.Sprintf("%s:%s", *domain, *port)
+        if *ipaddr != "" {
+            address = fmt.Sprintf("%s:%s", *ipaddr, *port)
+        }
+
+        // Create a TLS connection with the specified server name
+        conn, err := tls.Dial("tcp", address, &tls.Config{
+            InsecureSkipVerify: true,
+            ServerName:         *domain, // Specify the server name
+        })
+        if err != nil {
+            log.Printf("Failed to connect: %v", err)
+            os.Exit(1)
+        }
+        defer conn.Close()
+
+        // Retrieve certificates
+        certs := conn.ConnectionState().PeerCertificates
+        if len(certs) == 0 {
+            log.Fatal("No certificates found")
+        }
+
+        cert = certs[0] // Take the first certificate, which is usually the end-entity certificate
     }
 
-    // Create a TLS connection with the specified server name
-    conn, err := tls.Dial("tcp", address, &tls.Config{
-        InsecureSkipVerify: true,
-        ServerName:         *domain, // Specify the server name
-    })
-    if err != nil {
-        log.Printf("Failed to connect: %v", err)
-        os.Exit(1)
-    }
-    defer conn.Close()
-
-    // Retrieve certificates
-    certs := conn.ConnectionState().PeerCertificates
-    if len(certs) == 0 {
-        log.Fatal("No certificates found")
-    }
-
-    // Output information about the end certificate
-    cert := certs[0] // Take the first certificate, which is usually the end-entity certificate
+    // Calculate days remaining until expiration
     daysRemaining := int(time.Until(cert.NotAfter).Hours() / 24)
-
-    // Get the remote address
-    remoteAddr := conn.RemoteAddr().String()
 
     // Print certificate information based on the short flag
     if *short {
         fmt.Println(daysRemaining)
     } else {
-        fmt.Printf("Certificate for %s\n", *domain)
+        fmt.Printf("Certificate for %s\n", cert.Subject.CommonName)
         fmt.Printf("Subject: %s\n", cert.Subject)
         fmt.Printf("Issuer: %s\n", cert.Issuer)
         fmt.Printf("Expires on: %s\n", cert.NotAfter)
         fmt.Printf("Days remaining: %d\n", daysRemaining)
-        fmt.Printf("Connected from IP: %s\n", remoteAddr) // Output the remote IP address
     }
+}
+
+// loadCertificate loads a certificate from a file
+func loadCertificate(certFile string) (*x509.Certificate, error) {
+    certPEM, err := os.ReadFile(certFile)
+    if err != nil {
+        return nil, err
+    }
+
+    block, _ := pem.Decode(certPEM)
+    if block == nil || block.Type != "CERTIFICATE" {
+        return nil, fmt.Errorf("failed to parse certificate")
+    }
+
+    cert, err := x509.ParseCertificate(block.Bytes)
+    if err != nil {
+        return nil, err
+    }
+
+    return cert, nil
 }
